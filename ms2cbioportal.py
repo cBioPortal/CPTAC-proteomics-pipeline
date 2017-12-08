ff46e569f42bb627af7acc8ba01775d12c5fed56
pamela-wu <pamela.wu@nyumc.org>
2017-12-08 13:53:01 -0500

    added tests

--- ms2cbioportal.py
+++ ms2cbioportal.py
@@ -10,9 +10,21 @@ class MSTable(object):
     
     __metaclass__  = abc.ABCMeta
     
-    def __init__(self, filenames):
+    def __init__(self, filename, sample_regex, ptm_type=None):
         self.df = None
-        self.load_data(filenames)
+        
+        # read in data from csv file
+        self.load_data(filename)
+        
+        # perform tests for data columns and data length
+        self.check_data()
+        
+        # clean and format the data
+        self.format_genes(ptm_type)
+        self.clean_rows()
+        self.clean_columns(sample_regex)
+        self.average_duplicates()
+        self.clean_values()
     
     def __str__(self):
         if self.df:
@@ -20,18 +32,14 @@ class MSTable(object):
         else:
             return str(pd.DataFrame())
     
-    def load_data(self, filenames):
-        dfs = []
-        for fname in filenames:
-            try:
-                df = pd.read_csv(fname, sep='\t', header=0)
-                dfs.append(df)
-            except OSError:
-                print('File {0} not found. Could not be converted into a Pandas dataframe.').format(fname)
-                raise
-        self.df = pd.concat(dfs, axis=0)
+    def load_data(self, filename):
+        try:
+            self.df = pd.read_csv(filename, sep='\t', header=0)
+        except OSError:
+            print('File {0} not found. Could not be converted into a Pandas dataframe.').format(filename)
+            raise
         return
-    
+        
     def clean_columns(self, sample_regex):
         sample_columns = [col for col in self.df.columns if re.match(sample_regex, col)]
         if not sample_columns:
@@ -40,11 +48,16 @@ class MSTable(object):
         return
     
     @abc.abstractmethod
+    def check_data(self):
+        return
+    
+    @abc.abstractmethod
     def clean_rows(self):
         return
     
     @abc.abstractmethod
     def format_genes(self):
+        return
     
     def clean_values(self):
         self.df.replace([np.inf, -np.inf], np.nan, inplace=True)
@@ -63,13 +76,22 @@ class MSTable(object):
         self.df.columns = [renamer(col) for col in self.df.columns]
         return
     
+    def vertical_concat(self, table):
+        self.df = pd.concat((self.df, table.df), axis=0)
+    
     def write_csv(self, filename):
         self.df.to_csv(filename, sep='\t', header=True, index=True)
+        return
 
 
 class CDAPTable(MSTable):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+    
+    def check_data(self):
+        assert len(self.df.index) > 0
+        assert 'Gene' in self.df.columns
+        return
     
     def clean_rows(self):
         self.df = self.df[~self.df['Gene'].str.contains('Mean|Median|StdDev', na=True)]
@@ -111,6 +133,13 @@ class MaxQuantProteomeTable(MSTable):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     
+    def check_data(self):
+        assert len(self.df.index) > 0
+        assert all(col in self.df.columns for col in ('Protein IDs', 
+                                                      'Gene names', 
+                                                      'Q-value'))
+        return
+    
     def clean_rows(self):
         self.df = self.df[~self.df['Protein IDs'].str.contains('REV|CON', na=True)]
         self.df = self.df[pd.notnull(self.df['Gene names'])]
@@ -128,13 +157,22 @@ class MaxQuantPTMTable(MSTable):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     
-    def clean_rows(self, exp_type):
+    def check_data(self):
+        assert len(self.df.index) > 0
+        assert all(col in self.df.columns for col in ('Protein', 
+                                                      'Amino acid', 
+                                                      'Positions within proteins', 
+                                                      'Gene names', 
+                                                      'Localization prob'))
+        return
+    
+    def clean_rows(self):
         self.df = self.df[~self.df['Protein'].str.contains('REV|CON', na=True)]
         self.df = self.df[pd.notnull(self.df['Gene names'])]
         self.df = self.df[self.df['Localization prob'] > 0.75]
         return
     
-    def format_genes(self, exp_type, ptm_type):
+    def format_genes(self, ptm_type):
         prot_pos = [p.split(';')[0] for p in self.df['Positions within proteins']]
         aa_col = self.df['Amino acid'].tolist()
         genes = [g.split(';')[0] for g in self.df['Gene names']]
@@ -160,10 +198,26 @@ class MSMeta(object):
     def __init__(self, cancer_id, prot_file):
         self.text = META_TEXT.format(cancer_id, prot_file)
     
+    def __str__(self):
+        return self.text
+    
     def write(self, filename):
         with open(filename, 'w') as f:
             f.write(self.text)
+        return
 
 
-
-
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='This script converts proteome abundance data and PTM data from CPTAC or MaxQuant into importable text files for cBioPortal. It will also output a meta file for use with cbioportalImporter.py. Use for one tissue type (i.e. breast cancer) at a time.')
+    
+    parser.add_argument('--proteome-files', 
+        metavar='<filename_1,filename_2,filename_n>', 
+        help='comma-separated list of protein abundance filenames', 
+        required=True)
+    parser.add_argument('--proteome-pipeline', 
+        choices=('itraq', 'precursor_area'), 
+        help='processing pipeline (i.e. "itraq" for Breast and Ovarian, "precursor_area for Colon")', 
+        required=True)
+    
+    args = parser.parse_args()
